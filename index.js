a132eb2cf6993d1822fefd1639aa5c4502bd08ba
Stuart McHattie <stuart.mchattie@tessella.com>
2018-04-25 09:52:04 +0100

    Change tabs to spaces in index.js

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
 'use strict';
 
-var logger		= require('winston'),
+var logger        = require('winston'),
 
-	RtmClient 	= require('@slack/client').RtmClient,
-	WebClient 	= require('@slack/client').WebClient,
-	MemoryDataStore = require('@slack/client').MemoryDataStore,
+  RtmClient       = require('@slack/client').RtmClient,
+  WebClient       = require('@slack/client').WebClient,
+  MemoryDataStore = require('@slack/client').MemoryDataStore,
 
-	RTM_EVENTS	= require('@slack/client').RTM_EVENTS,
-	CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS,
+  RTM_EVENTS      = require('@slack/client').RTM_EVENTS,
+  CLIENT_EVENTS   = require('@slack/client').CLIENT_EVENTS,
 
-	util = {
-		config: require('./util/config'),
-		command: require('./util/command')
-	},
+  util = {
+    config: require('./util/config'),
+    command: require('./util/command')
+  },
 
-	dispatcher	= require('./dispatcher'),
-	rtmClient, webClient;
+  dispatcher = require('./dispatcher'),
+  rtmClient, webClient;
 
 var _handleMessage = function (message) {
-	dispatcher.handle(message);
+  dispatcher.handle(message);
 }
 
 var _startClient = function () {
 
-	if (!rtmClient) {
-		throw new Error('Slack RTM client not initialized');
-	}
+  if (!rtmClient) {
+    throw new Error('Slack RTM client not initialized');
+  }
 
-	logger.info('Starting RTM client.');
+  logger.info('Starting RTM client.');
 
-	dispatcher.init(rtmClient)
+  dispatcher.init(rtmClient)
 
-	rtmClient.on('connected', function () {
-		logger.info('RTM client connected.');
-		rtmClient.addListener('message', _handleMessage);
-	});
+  rtmClient.on('connected', function () {
+    logger.info('RTM client connected.');
+    rtmClient.addListener('message', _handleMessage);
+  });
 
-	rtmClient.on('disconnected', function () {
-		logger.info('RTM client disconnected.');
-		rtmClient.removeListener('message', _handleMessage);
-	});
+  rtmClient.on('disconnected', function () {
+    logger.info('RTM client disconnected.');
+    rtmClient.removeListener('message', _handleMessage);
+  });
 
-	rtmClient.start();
+  rtmClient.start();
 };
 
 var getRTMClient = function () {
-	return rtmClient;
+  return rtmClient;
 };
 
 var getWebClient = function () {
-	return webClient;
+  return webClient;
 };
 
 var init = function (token, opts, config) {
 
-	if (!token) {
-		throw new Error('slack token not passed with opts in init');
-	}
+  if (!token) {
+    throw new Error('slack token not passed with opts in init');
+  }
 
-	// set defaults if not provided
-	opts.autoReconnect = opts.autoReconnect || true;
-	opts.logLevel = opts.logLevel || 'error';
-	opts.dataStore = new MemoryDataStore({});
+  // set defaults if not provided
+  opts.autoReconnect = opts.autoReconnect || true;
+  opts.logLevel = opts.logLevel || 'error';
+  opts.dataStore = new MemoryDataStore({});
 
-	rtmClient = new RtmClient(token, opts);
-	webClient = new WebClient(token, opts);
+  rtmClient = new RtmClient(token, opts);
+  webClient = new WebClient(token, opts);
 
-	util.config.init(config);
+  util.config.init(config);
 
-	_startClient();
+  _startClient();
 };
 
 
-exports.init 				= init;
-exports.getCommands 		= util.command.getCommandObjects;
-exports.getResponses 		= util.command.getResponseObjects;
-exports.getRTMClient 		= getRTMClient;
-exports.getWebClient		= getWebClient;
+exports.init            = init;
+exports.getCommands     = util.command.getCommandObjects;
+exports.getResponses    = util.command.getResponseObjects;
+exports.getRTMClient    = getRTMClient;
+exports.getWebClient    = getWebClient;
